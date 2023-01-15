559bb2cc01ef9aeeead6028a6bb935952a042a22
aleksandrsaitov <saa4981@gmail.com>
2023-01-15 04:48:02 +0300

    commit

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,14 +1,14 @@
 
-public class HelloWorld {
- 
+public class Cat {
+    public Cat() {
+    }
     private String message;
- 
+
     public String getMessage() {
         return message;
     }
- 
+
     public void setMessage(String message) {
         this.message = message;
     }
-     
 }
